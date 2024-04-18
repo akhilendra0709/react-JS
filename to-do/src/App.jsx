de23267051f04360ba13061539cf67abcc889dae
akhilendra0709 <155439162+akhilendra0709@users.noreply.github.com>
2024-04-19 00:28:44 +0530

    added a little personal touch to the UI

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -2,15 +2,12 @@ import { useState, useEffect } from "react";
 import { TodoProvider } from "./contexts";
 import TodoForm from "./components/TodoForm";
 import TodoItem from "./components/TodoItem";
-import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([]);
 
   const addTodo = (todo) => {
-    setTodos((prev) => {
-      [...prev, { id: Date.now(), ...todo }];
-    });
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
   };
 
   const updateTodo = (id, todo) => {
@@ -26,7 +23,9 @@ function App() {
   const toggleComplete = (id) => {
     setTodos((prev) =>
       prev.map((prevTodo) =>
-        prevTodo.id === id ? { ...prevTodo, checked: !prevTodo } : prevTodo
+        prevTodo.id === id
+          ? { ...prevTodo, checked: !prevTodo.checked }
+          : prevTodo
       )
     );
   };
@@ -46,17 +45,17 @@ function App() {
     <TodoProvider
       value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
     >
-      <div className="bg-[#172842] min-h-screen py-8">
+      <div className="bg-black min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
-            Manage Your Todos
+          <h1 className="text-6xl font-bold text-center mb-8 p-4 mt-2 hover:text-7xl transition-all ease-in-out duration-300 hover:text-purple-600">
+            Todo Manager
           </h1>
           <div className="mb-4">
             <TodoForm />
           </div>
           <div className="flex flex-wrap gap-y-3">
             {todos.map((todo) => (
-              <div className="w-full" key={todo.id}>
+              <div key={todo.id} className="w-full">
                 <TodoItem todo={todo} />
               </div>
             ))}
--- to-do/src/components/TodoForm.jsx
+++ to-do/src/components/TodoForm.jsx
@@ -6,10 +6,11 @@ function TodoForm() {
   const { addTodo } = useTodo();
 
   const add = (e) => {
-    e.preventDefault();
+    e.preventDefault;
     if (!todo) return;
     addTodo({ todo, checked: false });
     setTodo("");
+    console.log("done");
   };
   return (
     <form onSubmit={add} className="flex">
@@ -19,12 +20,12 @@ function TodoForm() {
         className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
         value={todo}
         onChange={(e) => {
-          e.target.value;
+          setTodo(e.target.value)
         }}
       />
       <button
         type="submit"
-        className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+        className="rounded-r-lg px-3 py-1 bg-purple-600 text-white shrink-0 hover:font-semibold hover:px-5 transition-all ease-in"
       >
         Add
       </button>
