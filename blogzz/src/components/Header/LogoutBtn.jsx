c5eba346ac09ca8c1724826dd6ecde8a3acfdb53
Akhilendra Pratap Singh Chandel <155439162+akhilendra0709@users.noreply.github.com>
2024-07-29 12:03:27 +0530

    build components

---
+++ blogzz/src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/configuration";
+import { logout } from "../../store/authSlice";
+
+function LogoutBtn() {
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    authService
+      .logout()
+      .then(() => {
+        dispatch(logout());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <button className="inline-block px-6 py-2 hover:bg-blue-200 rounded-full"
+    onClick={handleLogout}>
+      Logout
+    </button>
+  );
+}
+
+export default LogoutBtn;
